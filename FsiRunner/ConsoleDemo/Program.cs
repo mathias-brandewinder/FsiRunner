2911b7ff557eeca2fba486165121503771a7cd2c
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2012-12-01 18:14:01 -0800

    ConsoleDemo: added comments

--- FsiRunner/ConsoleDemo/Program.cs
+++ FsiRunner/ConsoleDemo/Program.cs
@@ -1,25 +1,31 @@
 ﻿namespace ConsoleDemo
 {
     using System;
+    using System.Diagnostics;
     using ClearLines.FsiRunner;
 
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Beginning");
-
+            // This is the path to FSI.EXE on my machine, adjust accordingly
             var fsiPath = @"C:\Program Files (x86)\Microsoft F#\v4.0\fsi.exe";
             var session = new FsiSession(fsiPath);
 
+            // start the session and hook the listeners
             session.Start();
             session.OutputReceived += OnOutputReceived;
             session.ErrorReceived += OnErrorReceived;
 
+            // Send some trivial code to FSI and evaluate
             var code = @"let x = 42";
             session.AddLine(code);
             session.Evaluate();
 
+            // Send a code block of 4 lines, using whitespace
+            // note how x, which was declared previously,
+            // is used in f as a closure, and still available.
             var line1 = @"let f y = x + y";
             var line2 = @"let z =";
             var line3 = @"   [ 1; 2; 3]";
@@ -31,24 +37,29 @@
             session.AddLine(line4);
             session.Evaluate();
 
+            // random "code" which is definitely not F#
+            // nothing crashes but we don't get any output?
             var error1 = "Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn";
             session.AddLine(error1);
             session.Evaluate();
 
+            // In spite of invoking Cthulhu before,
+            // our session is still healthy and evaluates this
             var code2 = @"let c = 123";
             session.AddLine(code2);
             session.Evaluate();
 
+            // wait for user to type [ENTER] to close
             Console.ReadLine();
         }
 
-        static void OnOutputReceived(object sender, System.Diagnostics.DataReceivedEventArgs e)
+        private static void OnOutputReceived(object sender, DataReceivedEventArgs e)
         {
             Console.WriteLine("FSI has happy news:");
             Console.WriteLine(e.Data);
         }
 
-        static void OnErrorReceived(object sender, System.Diagnostics.DataReceivedEventArgs e)
+        private static void OnErrorReceived(object sender, DataReceivedEventArgs e)
         {
             Console.WriteLine("FSI has bad news:");
             Console.WriteLine(e.Data);
